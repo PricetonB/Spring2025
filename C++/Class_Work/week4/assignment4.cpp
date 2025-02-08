ba9aa02b4476f91655f4bc6f354bc15aac476f20
Priceton Braswell <pricetonbraswell@gmail.com>
2025-02-08 05:39:15 +0000

    cpp week 4

---
+++ C++/Class_Work/week4/assignment4.cpp
@@ -0,0 +1,260 @@
+//Priceton Braswell
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------
+
+// person class
+
+
+#include <cmath>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+
+
+class Person {
+private:
+  std::string m_sex;
+  std::string m_first_name;
+  int m_weight_class_id;   // overweight = 0, probably not overweight = 1, normal weight = 2, under weight = 3
+  int m_height_feet;
+  int m_height_inches;
+  int m_body_fat_percent;
+  int m_id;
+  float m_weight_pounds;
+  float m_bmi;
+
+
+  void m_calculate_bmi(int feet, int inches, float pounds) {
+    m_bmi = pounds / ((feet * 12 + inches) * (feet * 12 + inches)) * 703;
+  }
+
+   void m_calculate_the_fatness(float bmi, int body_fat_percent, std::string sex) {
+     if (bmi > 25) {
+       if (sex == "male" && body_fat_percent > 24) {
+         m_weight_class_id = 0;
+         return;
+       }
+       if (sex == "female" && body_fat_percent > 31) {
+        m_weight_class_id = 0;
+         return;
+       }
+       m_weight_class_id = 1;
+       return;
+     }
+     if (bmi > 18.5) {
+       m_weight_class_id = 2;
+       return;
+     }
+     m_weight_class_id = 3;
+     return;
+   }
+
+
+
+
+
+
+public:
+  Person(std::string name, int height_inches, int height_feet, std::string sex, float pounds, int fat_percent, int id)
+    : m_first_name(name), 
+      m_height_inches(height_inches), 
+      m_height_feet(height_feet),
+      m_sex(sex), 
+      m_weight_pounds(pounds),
+      m_body_fat_percent(fat_percent),
+      m_id(id){
+    m_calculate_bmi(m_height_feet, m_height_inches, m_weight_pounds);
+    m_calculate_the_fatness(m_bmi, m_body_fat_percent, m_sex);
+  }
+
+  std::string get_sex() {
+    return m_sex;
+  }
+
+  std::string get_first_name() {
+    return m_first_name;
+  }
+
+  int get_height_feet() {
+    return m_height_feet;
+  }
+
+  int get_height_inches() {
+    return m_height_inches;
+  }
+
+  int get_body_fat_percent() {
+    return m_body_fat_percent;
+  }
+
+  int get_person_id() {
+    return m_id;
+  }
+
+  float get_weight() {
+    return m_weight_pounds;
+  }
+
+  float get_bmi() {
+    return m_bmi;
+  }
+
+  int get_weight_class_id() {
+    return m_weight_class_id;
+  }
+
+
+};
+
+  //---------------------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------------------------
+
+
+
+//group class
+//public methods: add person, print person info, print group info, -
+
+//private methods: calculate_male_bmi_avg, calculate_female_bmi_avg, calculate_group_bmi_avg, 
+
+
+
+class Group {
+private:
+
+
+  int m_group_count;
+  int m_male_count;
+  int m_female_count;
+  float m_group_bmi_avg;
+  float m_male_bmi_avg;
+  float m_female_bmi_avg;
+  std::vector<Person> m_people;
+  std::unordered_map<int, int> m_weight_class_counts = {{0,0}, {1,0}, {2,0}, {3,0}};   // weight_class_id : number of people in that weight class 
+  std::string m_weight_class_names[4] = {"overweight", "probably not overweight", "normal", "underweight" };  
+
+  
+
+  void m_print_weight_class_percentages() {
+    return;
+  }
+
+  void m_print_person_info(Person person) {
+    std::cout << "Name: " << person.get_first_name() << "\n";
+    std::cout << "Height: " << person.get_height_feet() << "," << person.get_height_inches() << "\n";
+    std::cout << "Weight: " << person.get_weight() << "\n";
+    std::cout << "Body Fat Percentage: " << person.get_body_fat_percent() << "\n";
+    std::cout << "Sex: " << person.get_sex() << "\n";
+    std::cout << "BMI: " << person.get_bmi() << "\n";
+    std::cout << "Weight Class: " << person.get_weight_class_id() << "\n";
+  }
+
+public:
+
+
+  void print_group_info() {
+    if (m_group_count == 0) {
+      std::cout << "there are no people in the group \n";
+      return;
+    }
+
+    std::cout << " Number of people in group:";
+    std::cout << " Group BMI average: ";
+    std::cout << " Male BMI average: ";
+    std::cout << "Female BMI average: ";
+    m_print_weight_class_percentages();
+  }
+
+  void print_person_info(int person_id) {
+    m_print_person_info(m_people[person_id]);
+  }
+
+
+
+  void add_person () {
+    std::string name;
+    int feet;
+    int inches;
+    float pounds;
+    int body_fat_percent;
+    std::string sex;
+    int person_id;
+
+    
+    std::cout << "please enter your first name "  << "\n";
+    std::cin >> name; 
+
+    std::cout << "please enter your height in feet "  << "\n";
+    std::cin >> feet ; 
+
+    std::cout << "how many inches on top of those feet? "  << "\n";
+    std::cin >> inches; 
+
+    std::cout << "please enter how many pounds you are "  << "\n";
+    std::cin >> pounds; 
+
+    std::cout << "please enter your body fat percent "  << "\n";
+    std::cin >> body_fat_percent; 
+
+    std::cout << "male or female? "  << "\n";
+    std::cin >> sex; 
+
+    m_group_count += 1;
+    person_id = m_group_count;
+
+
+    if (sex == "male") {
+      m_male_count += 1;
+    }
+    if (sex == "female") {
+      m_female_count +=1;
+    }
+
+
+    Person person_to_add = Person(name, inches, feet, sex, pounds, body_fat_percent, person_id);
+    m_people.push_back(person_to_add);
+    m_weight_class_counts[person_to_add.get_weight_class_id()]++;
+  }
+
+};
+
+
+
+//  Person(std::string name, int height_inches, int height_feet, std::string sex, float pounds, int fat_percent, int id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main (int argc, char *argv[]) {
+  Person p1 = Person("mike", 10, 5, "male", 150.0, 15, 1);
+
+  print_person_info(p1);
+
+  return 0;
+}
+
+
+//Person(std::string name, int height_inches, int height_feet, std::string sex, float pounds, int fat_percent, int id)
+
---
+++ C++/Class_Work/week4/build.sh
@@ -0,0 +1,1 @@
+g++ assignment4.cpp -o assignment4
